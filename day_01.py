cebf790d87bdda97fcc689973bc648de3a9a9ebe
Cristóbal Sánchez <tristobal@gmail.com>
2022-12-27 12:21:03 -0300

    Adds days 01. Adds README.md.

--- day_01.py
+++ day_01.py
@@ -17,10 +17,10 @@ i = 4
 d = 4.0
 s = 'HackerRank '
 # Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
 i_input = int(input())
 d_input = float(input())
 s_input = input()
-# Read and save an integer, double, and String to your variables.
 
 # Print the sum of both integer variables on a new line.
 print(i + i_input)
